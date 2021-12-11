038255b69704228f4c3fb2687e029bf8a7ca27d7
René Schmidt <schmidt2800@gmail.com>
2021-12-11 16:30:44 +0100

    routing tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
-  oddNumbers = [1, 3, 5];
-  evenNumbers = [2, 4];
-  onlyOdd = false;
-  value = 5;
 }
