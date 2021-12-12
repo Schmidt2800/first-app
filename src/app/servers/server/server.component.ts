f090f3313121d241bed441ec61e52b89a32bbd4b
René Schmidt <schmidt2800@gmail.com>
2021-12-12 12:50:48 +0100

    wip

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { ServersService } from '../servers.service';
 
@@ -8,12 +10,19 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-
-  constructor(private serversService: ServersService) { }
+  server: { id: number, name: string, status: string };
+  paramsSubscription: Subscription;
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
+
   }
 
 }
