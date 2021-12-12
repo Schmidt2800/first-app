3eaf1ac4b01e4620c324e13b8aaf4386071f13fe
René Schmidt <schmidt2800@gmail.com>
2021-12-12 13:48:58 +0100

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
@@ -9,6 +8,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -18,7 +18,7 @@ const appRoutes: Routes = [
         ]
     },
     {
-        path: 'servers', component: ServersComponent, children: [
+        path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
             { path: ':id', component: ServerComponent },
             { path: ':id/edit', component: EditServerComponent },
         ]
